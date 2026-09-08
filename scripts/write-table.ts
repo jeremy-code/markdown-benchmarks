f3e65efcab6f143b8b5f0161529a5ab2011e9279
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-08 00:16:48 -0700

    docs: update README.md with tinybench-formatted results

--- scripts/write-table.ts
+++ scripts/write-table.ts
@@ -1,4 +1,11 @@
 import { tablemark } from "tablemark";
+import {
+  Bench,
+  type ConsoleTableConverter,
+  formatNumber,
+  mToNs,
+  type Task,
+} from "tinybench";
 
 //github.com/vitest-dev/vitest/blob/d9ea973140d267ca4702deb32aa35c0afb960e52/packages/vitest/src/node/reporters/renderers/benchmark-table.ts#L17
 function formatBenchNumber(number: number): string {
@@ -8,7 +15,7 @@ function formatBenchNumber(number: number): string {
   );
 }
 
-const table = tablemark(
+const vitestTable = tablemark(
   // https://github.com/vitest-dev/vitest/blob/d9ea973140d267ca4702deb32aa35c0afb960e52/packages/vitest/src/node/reporters/renderers/benchmark-table.ts
   (
     await import("../test-output.json", { with: { type: "json" } })
@@ -27,4 +34,23 @@ const table = tablemark(
   ),
 );
 
-console.log(table);
+console.log(vitestTable);
+
+// https://github.com/tinylibs/tinybench/blob/147c2ac3ae7b30aa1c4a9223068b62d806d14353/src/utils.ts#L738
+const tinyBenchTable = tablemark(
+  // https://github.com/vitest-dev/vitest/blob/d9ea973140d267ca4702deb32aa35c0afb960e52/packages/vitest/src/node/reporters/renderers/benchmark-table.ts
+  (
+    await import("../test-output.json", { with: { type: "json" } })
+  ).default.testResults[0]!.assertionResults[0]!.benchmarks[0]!.tasks.map(
+    (task) => ({
+      "Task name": task.name,
+      "Latency avg (ns)": `${formatNumber(mToNs(task.latency.mean))} \xb1 ${formatNumber(task.latency.rme)}%`,
+      "Latency med (ns)": `${formatNumber(mToNs(task.latency.p50))} \xb1 ${formatNumber(mToNs(task.latency.mad))}`,
+      "Throughput avg (ops/s)": `${Math.round(task.throughput.mean).toString()} \xb1 ${formatNumber(task.throughput.rme)}%`,
+      "Throughput med (ops/s)": `${Math.round(task.throughput.p50).toString()} \xb1 ${Math.round(task.throughput.mad).toString()}`,
+      Samples: task.latency.samplesCount,
+    }),
+  ),
+);
+
+console.log(tinyBenchTable);
